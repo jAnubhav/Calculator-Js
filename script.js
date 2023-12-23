4ae342158b152f9956d3fa4674b03f6861f2db99
Anubhav Jain <87580533+jAnubhav@users.noreply.github.com>
2023-12-23 17:41:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
+const checkOperator = text => "/*-+".includes(text);
+
 const addInEntry = text => {
-    if (entry.innerText == "Error") allClear();
+    if (entry.innerText == "Error" || text == "AC") allClear();
+
     if (text == "DEL") delText();
-    else if (text == "AC") allClear();
-    else if (entry.innerText == "0") { if (!"/*-+".includes(text)) entry.innerText = text; }
-    else if (entry.innerText.length < 15) entry.innerText += text;
+    else if (entry.innerText != "0" && entry.innerText.length < 15) {
+        if (checkOperator(text) && "/*-+".includes(entry.innerText[entry.innerText.length - 1])) delText();
+        entry.innerText += text;
+    } else {
+        if (!checkOperator(text)) entry.innerText = text;
+    }
 }
+
 const delText = () => {
     entry.innerText = entry.innerText.slice(0, entry.innerText.length - 1);
     if (entry.innerText.length == 0) allClear();
@@ -13,7 +20,7 @@ const delText = () => {
 const allClear = () => entry.innerText = 0;
 
 const findResult = () => {
-    try { entry.innerText = eval(entry.innerText); if(entry.innerText.includes("Infinity")) throw e}
+    try { entry.innerText = eval(entry.innerText); if (entry.innerText.includes("Infinity")) throw e }
     catch (e) { entry.innerText = "Error"; }
 }
 
